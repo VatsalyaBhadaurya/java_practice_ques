d207daa13903a16a8d688471863b0b27b05143fb
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-05-28 13:18:34 +0530

    Update input_pract_ques.java

--- input_pract_ques.java
+++ input_pract_ques.java
@@ -1,9 +1,23 @@
+// Q1) Write a program to take two integer inputs from user and print sum and product of them.
+
 import java.util.Scanner;
 
 public class io_practqs {
     public static void main(String[] args) {
+
         System.out.println("Enter no 1");
+        Scanner num1 = new Scanner(System.in);
+        float x = num1.nextFloat();
+
         System.out.println("Enter no 2");
-        
+        Scanner num2 = new Scanner(System.in);
+        float y = num2.nextFloat();
+
+        double sum = x+y;
+        System.out.println("Sum = "+sum);
+
+        double prod = x*y;
+        System.out.println("Product = "+prod);
+
     }
 }
