ef39958177c2445ffc641180177e750859320a43
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-05-28 13:06:27 +0530

    Create input_pract_ques.java

---
+++ input_pract_ques.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class io_practqs {
+    public static void main(String[] args) {
+        System.out.println("Enter no 1");
+        System.out.println("Enter no 2");
+        
+    }
+}
