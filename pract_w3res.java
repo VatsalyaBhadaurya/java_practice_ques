58767671a3df213370c5007930860110f13a2117
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-05-29 22:51:03 +0530

    Create pract_w3res.java

---
+++ pract_w3res.java
@@ -0,0 +1,14 @@
+// QUESTIONS TAKEN FROM https://www.w3resource.com/java-exercises/basic/index.php
+
+// Q1) Write a Java program to print 'Hello' on screen and your name on a separate line Expected Output :Hello     Alexandra Abramov
+
+public class prac_ques_w3 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello\nAlexandra Abramov");
+
+    }
+}
+
+// Q2) Write a Java program to print the sum of two numbers.Test Data: 74 + 36
+
