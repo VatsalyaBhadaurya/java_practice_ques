ab551c88be1a176692d907905f854ae1ee46b549
Vatsalya Bhadaurya <150329198+VatsalyaBhadaurya@users.noreply.github.com>
2024-06-04 19:01:01 +0530

    Update pract_Conditional_w3res.java

--- pract_Conditional_w3res.java
+++ pract_Conditional_w3res.java
@@ -21,3 +21,44 @@ public class conditionals {
     }
 }
 
+// Q3) Write a Java program that takes three numbers from the user and prints the greatest number.
+
+import java.util.Scanner;
+
+public class conditionals {
+    public static void main(String[] args) {
+
+        System.out.println("Enter num1 :");
+        Scanner n = new Scanner(System.in);
+        float n1 = n.nextFloat();
+
+        System.out.println("Enter num2 :");
+        Scanner l = new Scanner(System.in);
+        float n2 = l.nextFloat();
+
+        System.out.println("Enter num3 :");
+        Scanner m = new Scanner(System.in);
+        float n3 = m.nextFloat();
+
+        if (n1 == n2 && n2 == n3 && n3 == n1){
+            System.out.println("All are equal");
+        }
+
+        if (n1 > n2 && n2 > n3 && n3 < n1){
+            System.out.println(n1+" Is greatest");
+        }
+
+        if (n2 > n1 && n1 > n3 && n3 < n2){
+            System.out.println(n2+" is greatest");
+        }
+
+        if (n3 > n1 && n1 > n2 && n2 < n3){
+            System.out.println(n3+" is greatest");
+        }
+
+        if (n3 > n1 && n2 > n1 && n1 < n3){
+            System.out.println(n3+" is greatest");
+        }
+        
+    }
+}
